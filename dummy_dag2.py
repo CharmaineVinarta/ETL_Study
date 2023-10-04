072bbe57edb48c91b997c79fceaed49f5942cf07
Charmaine Vinarta <120625052+CharmaineVinarta@users.noreply.github.com>
2023-10-04 16:18:46 -0700

    Delete dummy_dag2.py

--- dummy_dag2.py
+++
@@ -1,8 +0,0 @@
-from IPython.display import Image, display
-
-# Specify the relative path to your PNG image file in your repository
-image_path = "1.png"
-
-# Display the image
-display(Image(filename=1))
-
